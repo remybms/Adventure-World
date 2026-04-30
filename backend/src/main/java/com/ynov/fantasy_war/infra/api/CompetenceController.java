3204d1809e78cbc01b4ce2dce0a8d874360162c1
remybms <112947392+remybms@users.noreply.github.com>
2026-04-30 10:02:54 +0200

    Create competence controller

---
+++ backend/src/main/java/com/ynov/fantasy_war/infra/api/CompetenceController.java
@@ -0,0 +1,58 @@
+package com.ynov.fantasy_war.infra.api;
+
+import com.ynov.fantasy_war.infrastructure.web.openapi.api.CompetencesApi;
+
+import java.util.UUID;
+
+public class CompetenceController implements CompetencesApi {
+
+    @Override
+    public void ajouterCompetenceAventurier(UUID aventurier, UUID competence){
+
+    }
+
+    @Override
+    public void creerCompetence(){
+
+    }
+
+    @Override
+    public void listerAventuriersParCompetence(UUID competence){
+
+    }
+
+    @Override
+    public void listerCompetences(){
+
+    }
+
+    @Override
+    public void listerCompetencesAventurier(UUID aventurier){
+
+    }
+
+    @Override
+    public void modifierCompetence(UUID id) {
+
+    }
+
+    @Override
+    public void obtenirCompetence(UUID id) {
+
+    }
+
+    @Override
+    public void obtenirCompetencesDisponibles(UUID id) {
+
+    }
+
+    @Override
+    public void retirerCompetenceAventurier(UUID id, UUID cId) {
+
+    }
+
+    @Override
+    public void supprimerCompetence(UUID id) {
+
+    }
+}
