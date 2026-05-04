97f7910edf690b4a662d5d79a98175949b54891f
remybms <112947392+remybms@users.noreply.github.com>
2026-05-04 10:49:12 +0200

    edit swagger and clean the code

--- backend/src/main/java/com/ynov/fantasy_war/services/aventurier/AventurierMapper.java
+++ backend/src/main/java/com/ynov/fantasy_war/services/aventurier/AventurierMapper.java
@@ -1,5 +1,7 @@
 package com.ynov.fantasy_war.services.aventurier;
 
+import com.ynov.fantasy_war.infra.bdd.entity.AventurierEntity;
+import com.ynov.fantasy_war.infra.bdd.entity.ClasseEntity;
 import com.ynov.fantasy_war.infrastructure.web.openapi.dto.AventurierDto;
 import com.ynov.fantasy_war.infrastructure.web.openapi.dto.AventurierPayload;
 
@@ -15,5 +17,20 @@ public class AventurierMapper {
         aventurier.setPerception(aventurierPayload.getPerception());
         aventurier.setClasse(AventurierDto.ClasseEnum.fromValue(aventurierPayload.getClasse().getValue()));
         aventurier.setNiveau(aventurierPayload.getNiveau());
+    }
+
+    public static AventurierDto toDto(AventurierEntity aventurier) {
+        AventurierDto aventurierResult = new AventurierDto();
+        aventurierResult.setId(aventurier.getId());
+        aventurierResult.setNom(aventurier.getNom());
+        aventurierResult.setDescription(aventurier.getDescription());
+        aventurierResult.setNiveau(aventurier.getNiveau());
+        ClasseEntity classeEntityResult = aventurier.getClasse();
+        AventurierDto.ClasseEnum classeResult = AventurierDto.ClasseEnum.valueOf(classeEntityResult.name());
+        aventurierResult.setClasse(classeResult);
+        aventurierResult.setPhysique(aventurier.getPhysique());
+        aventurierResult.setMental(aventurier.getMental());
+        aventurierResult.setPerception(aventurier.getPerception());
+        return aventurierResult;
     }
 }
