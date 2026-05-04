a6f6361affb58fbdf4beca064c545aa215bc271d
remybms <112947392+remybms@users.noreply.github.com>
2026-05-04 15:52:07 +0200

    edit homepage, and little edits here and there

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,54 +1,27 @@
 import { useState, useEffect } from 'react'
 import './App.css'
 import apiClient from '../AuthContext/apiClient'
-import type { AdventurerType, CustomJwtPayload } from './AdventurerType'
-import { jwtDecode } from 'jwt-decode'
-import Header from './Header'
+import type { AdventurerType } from './AdventurerType'
 
 function App() {
 
   const [adventurers, setAdventurers] = useState<AdventurerType[]>()
 
-  const token = localStorage.getItem("token")
-  let isAdmin = false;
-  if (token) {
-    try {
-      isAdmin = jwtDecode<CustomJwtPayload>(token).scope.includes("ADMIN");
-    } catch (error) {
-      console.log("Invalid token:", error);
-    }
-  }
 
-
-
-  const deleteAdventurer = async (idAdventurer: string) => {
-    try {
-      const response = await apiClient.delete(`/api/v1/aventuriers/${idAdventurer}`)
-      if (response.status != 204) {
-        throw new Error(`Response status: ${response.status}`);
-      }
-      const data = await response.data
-      console.log(data)
-      fetchData()
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-  const fetchData = async () => {
-    try {
-      const response = await apiClient.get(`/api/v1/aventuriers`)
-      if (response.status != 200) {
-        throw new Error(`Response status: ${response.status}`);
-      }
-      const data = await response.data
-      setAdventurers(data)
-    } catch (error) {
-      console.log(error)
-    }
-  }
 
   useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await apiClient.get(`/api/v1/aventuriers`)
+        if (response.status != 200) {
+          throw new Error(`Response status: ${response.status}`);
+        }
+        const data = await response.data
+        setAdventurers(data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
     fetchData()
   }, [])
 
@@ -58,27 +31,19 @@ function App() {
 
   return (
     <>
-      <Header />
       <div className='content'>
-        <h1>Adventure World</h1>
-        
-        <h2>Welcome to Adventure World</h2>
-        <h2>Please login to continue</h2>
-        
-        {adventurers.map((adventurer) => (
+        <h1>Welcome to Adventure World !</h1>
+
+        <div className='grid'>
+          {adventurers.map((adventurer) => (
           <div className='card'>
-            {isAdmin &&
-              <div className='adminButtons'>
-                <button onClick={() => deleteAdventurer(adventurer.id)}>Supprimer</button>
-                <a href={`/update-adventurer?id=${adventurer.id}`}>Modifier</a>
-              </div>
-            }
             <a href={`/aventurier?id=${adventurer.id}`}>
               <h2>{adventurer.nom}</h2>
-              <p>{adventurer.description}</p>
             </a>
           </div>
         ))}
+        </div>
+        
       </div>
 
     </>
